b8864cb4ebdb851c6f7225df2e71b259ceaee830
Jeremy Wynn <jeremy@jeremywynn.com>
2019-09-01 11:30:21 -0500

    Optimizing AWS Lambda performance in all-items.js function

--- functions/all-items.js
+++ functions/all-items.js
@@ -7,48 +7,38 @@ import {
   UserApiKeyCredential
 } from "mongodb-stitch-server-sdk";
 
+//Performance optimization Step 1: declare the database connection object outside the handler method
+let cachedDb = null;
+let dataDirectory = '';
+
+if (process.env.NETLIFY === 'true') {
+  dataDirectory = dataDirectory = '/tmp';
+}
+
+const client = Stitch.initializeDefaultAppClient(
+  "catalogue-fjarv",
+  new StitchAppClientConfiguration.Builder().withDataDirectory(dataDirectory).build()
+);
+
+const mongoClient = client.getServiceClient(
+  RemoteMongoClient.factory,
+  "mongodb-atlas"
+);
+
+const credential = new UserApiKeyCredential(process.env.MONGODB_API_KEY);
+
 const headers = {
   Accept: "application/json",
   "Content-Type": "application/json"
 };
 
 exports.handler = async (event, context, callback) => {
-  let dataDirectory = '';
-
-  if (Object.entries(context.clientContext).length != 0) {
-    dataDirectory = '/tmp';
-  }
-
-  const client = Stitch.initializeDefaultAppClient(
-    "catalogue-fjarv",
-    new StitchAppClientConfiguration.Builder().withDataDirectory(dataDirectory).build()
-  );
-
-  const mongoClient = client.getServiceClient(
-    RemoteMongoClient.factory,
-    "mongodb-atlas"
-  );
-
-  const credential = new UserApiKeyCredential(process.env.MONGODB_API_KEY);
 
   try {
-    const data = JSON.parse(event.body);
-    // let skipAmount = 18;
-    let skipAmount = 0;
-    if (data.skip) {
-      skipAmount = data.skip;
-    }
-    await client.auth.loginWithCredential(credential);
-    const db = mongoClient.db("catalogue");
-    const itemsCollection = db.collection("items");
-    // const itemsCount = await itemsCollection.count();
-    // console.log(itemsCount);
+    //Performance optimization Step 2: set context.callbackWaitsForEmptyEventLoop to false to prevent the Lambda function from waiting for all resources (such as the database connection) to be released before returning it
+    context.callbackWaitsForEmptyEventLoop = false;
 
-    // skipAmount 1040
-
-    const pipeline = [{$sort: {"_id": -1} }, { $skip: skipAmount }, { $limit: 10 }];
-    const items = await itemsCollection.aggregate(pipeline).toArray();
-
+    const items = await processEvent(event, context, callback);
     return {
       statusCode: 200,
       headers,
@@ -63,3 +53,63 @@ exports.handler = async (event, context, callback) => {
     };
   }
 };
+
+async function connectToDatabase(uri) {
+
+  try {
+    //Performance optimization Step 3: test that database connection exists and is valid
+    //before re-using it
+    if (cachedDb && cachedDb.serverConfig.isConnected()) {
+      console.log('=> using cached database instance');
+      return Promise.resolve(cachedDb);
+    }
+    await client.auth.loginWithCredential(credential);
+    const db = mongoClient.db("catalogue");
+    cachedDb = db;
+    return cachedDb;
+  }
+  catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+
+async function processEvent(event, context, callback) {
+  try {
+    const db = await connectToDatabase();
+    const result = await queryDatabase(db, event);
+    return result;
+  } catch (error) {
+    console.log(error); // output to netlify function log
+    return error;
+  }
+}
+
+async function queryDatabase(db, event) {   
+  var jsonContents = JSON.parse(JSON.stringify(event));
+  // const data = JSON.parse(event.body);
+  
+  //handling API Gateway input where the event is embedded into the 'body' element
+  if (event.body !== null && event.body !== undefined) {
+      console.log('retrieving payload from event.body');
+      jsonContents = JSON.parse(event.body);
+  }
+
+  console.log('query parameters: ', jsonContents);
+
+  let skipAmount = 0;
+  if (jsonContents.skip) {
+    skipAmount = jsonContents.skip;
+  }
+
+  try {
+    const itemsCollection = db.collection("items");
+    const pipeline = [{$sort: {"_id": -1} }, { $skip: skipAmount }, { $limit: 10 }];
+    const items = await itemsCollection.aggregate(pipeline).toArray();
+
+    return items;
+
+  } catch (error) {
+    return error;
+  }
+}
