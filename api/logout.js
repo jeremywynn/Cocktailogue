af4db5f233c4eb36d33bf130d900621422c20bf1
Jeremy Wynn <jeremy@jeremywynn.com>
2020-01-17 22:31:17 -0600

    Netlify SPA
    Netlify doesn't support Universal mode :(

--- api/logout.js
+++ api/logout.js
@@ -1,14 +1,13 @@
-/* eslint-disable prettier/prettier */
-import faunadb from "faunadb"
-require("dotenv").config()
+import faunadb from 'faunadb'
+require('dotenv').config()
 
 exports.handler = async (req, res) => {
-  const q = faunadb.query
-  const client = new faunadb.Client({
-    secret: process.env.FAUNADB_SERVER_SECRET
-  })
-  await client.query(q.Logout(true))
-  res.end()
+	const q = faunadb.query
+	const client = new faunadb.Client({
+		secret: process.env.FAUNADB_SERVER_SECRET
+	})
+	await client.query(q.Logout(true))
+	res.end()
 }
 
 exports.path = '/api/auth/logout'
